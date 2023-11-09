c47bc32641ee5c26c3d3e37c04addcadc0c790cd
gorkapb <gorkapineda@gmail.com>
2023-11-09 10:29:38 +0100

    Quick fixes

--- src/navigate.py
+++ src/navigate.py
@@ -16,7 +16,7 @@ driver = webdriver.Firefox(options=options)
 
 driver.get("https://es.aliexpress.com/")  # Go to the URL
 
-time.sleep(random.uniform(1, 3))
+time.sleep(random.uniform(1, 3))  # wait random time
 
 # Wait for the cookies acceptance button to load and click it
 try:
@@ -26,7 +26,7 @@ try:
 except:
     print("Cookies acceptance button not found")
 
-time.sleep(random.uniform(1, 3))
+time.sleep(random.uniform(1, 3))  # wait random time
 
 # Wait for the pop-up close button to load and click it
 try:
@@ -36,40 +36,40 @@ try:
 except:
     print("Pop-up close button not found")
 
-time.sleep(random.uniform(1, 3))
+time.sleep(random.uniform(1, 3))  # wait random time
 
 #  Get all the URLs from the dropdown (categories)
 dropdown_urls = driver.find_elements(By.CSS_SELECTOR, "ul.Categoey--categoryList--2QES_k6 a")
 for category_link in dropdown_urls:
-    url1 = category_link.get_attribute('href')  # Get the URL
+
     driver.execute_script("window.open('');")  # Open new tab
     driver.switch_to.window(driver.window_handles[-1])  # Switch to new tab
-    driver.get(url1)  # Go to the URL
+    driver.get(category_link.get_attribute('href'))  # Go to the URL
 
-    time.sleep(random.uniform(1, 3))
+    time.sleep(random.uniform(1, 3))  # wait random time
 
     #  Get all the URLs from the category (subcategories)
     category_urls = driver.find_elements(By.XPATH, '//div[@class="lv3Category--lv3CategoryBox--1Nts99Z"]/a')
     for subcategory_link in category_urls:
-        url2 = subcategory_link.get_attribute('href')  # Get the URL
+
         driver.execute_script("window.open('');")  # Open new tab
         driver.switch_to.window(driver.window_handles[-1])  # Switch to new tab
-        driver.get(url2)  # Go to the URL
+        driver.get(subcategory_link.get_attribute('href'))  # Go to the URL
 
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(1, 3))  # wait random time
 
         #  Get all the URLs from the subcategory (products)
         product_urls = driver.find_elements(By.CLASS_NAME, 'multi--container--1UZxxHY')
         for i, product_link in enumerate(product_urls):
+
             if i >= product_depth:  # Check product navigation depth
                 break
 
-            url3 = product_link.get_attribute('href')  # Get the URL
             driver.execute_script("window.open('');")  # Open new tab
             driver.switch_to.window(driver.window_handles[-1])  # Switch to new tab
-            driver.get(url3)  # Go to the URL
+            driver.get(product_link.get_attribute('href'))  # Go to the URL
 
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(1, 3))  # wait random time
 
             # PETTER CODE ####
 
@@ -78,17 +78,17 @@ for category_link in dropdown_urls:
             # Close the tab
             driver.close()
             driver.switch_to.window(driver.window_handles[-1])  # Switch to previous tab
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(1, 3))  # wait random time
 
         # Close the tab
         driver.close()
         driver.switch_to.window(driver.window_handles[-1])  # Switch to previous tab
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(1, 3))  # wait random time
 
     # Close the tab
     driver.close()
     driver.switch_to.window(driver.window_handles[-1])  # Switch to previous tab
-    time.sleep(random.uniform(1, 3))
+    time.sleep(random.uniform(1, 3))  # wait random time
 
 # Close the browser
 driver.quit()
