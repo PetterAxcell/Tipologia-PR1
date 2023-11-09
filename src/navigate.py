f04e38bd4883f75777e7f0dc195b093a02d20643
gorkapb <gorkapineda@gmail.com>
2023-11-09 11:06:00 +0100

    Quick fixes 3

--- src/navigate.py
+++ src/navigate.py
@@ -39,34 +39,34 @@ except:
 
 time.sleep(random.uniform(1, 3))  # wait random time
 
-#  Get all the URLs from the dropdown (categories)
-dropdown_urls = driver.find_elements(By.CSS_SELECTOR, "ul.Categoey--categoryList--2QES_k6 a")
-for category_link in dropdown_urls:
+#  Get all the anchors from the dropdown (categories)
+dropdown_anchors = driver.find_elements(By.CSS_SELECTOR, "ul.Categoey--categoryList--2QES_k6 a")
+for category_anchor in dropdown_anchors:
 
     driver.execute_script("window.open('');")  # Open new tab
     driver.switch_to.window(driver.window_handles[-1])  # Switch to new tab
-    driver.get(category_link.get_attribute('href'))  # Go to the URL
+    driver.get(category_anchor.get_attribute('href'))  # Go to the URL
     time.sleep(random.uniform(1, 3))  # wait random time
 
-    #  Get all the URLs from the category (subcategories)
-    category_urls = driver.find_elements(By.XPATH, '//div[@class="lv3Category--lv3CategoryBox--1Nts99Z"]/a')
-    for subcategory_link in category_urls:
+    #  Get all the anchors from the category (subcategories)
+    category_anchors = driver.find_elements(By.XPATH, '//div[@class="lv3Category--lv3CategoryBox--1Nts99Z"]/a')
+    for subcategory_anchor in category_anchors:
 
         driver.execute_script("window.open('');")  # Open new tab
         driver.switch_to.window(driver.window_handles[-1])  # Switch to new tab
-        driver.get(subcategory_link.get_attribute('href'))  # Go to the URL
+        driver.get(subcategory_anchor.get_attribute('href'))  # Go to the URL
         time.sleep(random.uniform(1, 3))  # wait random time
 
-        #  Get all the URLs from the subcategory (products)
-        product_urls = driver.find_elements(By.CLASS_NAME, 'multi--container--1UZxxHY')
-        for i, product_link in enumerate(product_urls):
+        #  Get all the anchors from the subcategory (products)
+        product_anchors = driver.find_elements(By.CLASS_NAME, 'multi--container--1UZxxHY')
+        for i, product_anchor in enumerate(product_anchors):
 
             if i >= product_depth:  # Check product navigation depth
                 break
 
             driver.execute_script("window.open('');")  # Open new tab
             driver.switch_to.window(driver.window_handles[-1])  # Switch to new tab
-            driver.get(product_link.get_attribute('href'))  # Go to the URL
+            driver.get(product_anchor.get_attribute('href'))  # Go to the URL
             time.sleep(random.uniform(1, 3))  # wait random time
 
             # PETTER CODE ####
