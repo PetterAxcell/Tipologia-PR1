a284c1bd4d644884bdf00a90ea350a82935de83d
gorkapb <gorkapineda@gmail.com>
2023-11-12 18:35:32 +0100

    save

--- source/settings.py
+++ source/settings.py
@@ -14,7 +14,7 @@ class Opt:
         self.options = Options()
 
     def get_driver(self, type_client="firefox"):
-        self.options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) "
+        self.options.set_preference("general.useragent.override", "Mozilla/5.1(Windows NT 10.0; Win64; x64; rv:89.0) "
                                                                   "Gecko/20100101 Firefox/89.0")
         if type_client == "firefox":
             return webdriver.Firefox(options=self.options)
